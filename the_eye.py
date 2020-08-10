08a95cc1889097e5f34b99fe655a5d188f27d137
Harm <mail@harmtemolder.nl>
2020-08-10 16:58:07 -0400

    Clean up print statements

--- the_eye.py
+++ the_eye.py
@@ -9,7 +9,6 @@ import gzip
 import itertools
 import json
 import os
-# import pickle
 import urllib2
 
 from bs4 import BeautifulSoup
@@ -18,13 +17,10 @@ class TheEye:
     def __init__(self,
                  base_url='https://the-eye.eu/public/Books/Calibre_Libraries/',
                  index_file='./the_eye.json.gz'):
-        # print('the_eye.py:TheEye:__init__: begin')
 
         self.base_url = base_url
         self.index_file = index_file
         self.load_index()
-
-        # print('the_eye.py:TheEye:__init__: complete')
 
     def load_index(self):
         """Tries to load a local index from a JSON file. Creates a new
@@ -32,17 +28,11 @@ class TheEye:
 
         :return: the index as a list of URLs pointing to files
         """
-        # print('the_eye.py:TheEye:load_index')
-
         if os.path.isfile(self.index_file):
             with gzip.open(self.index_file, mode='rb') as index_json:
                 self.index = json.loads(index_json.read())
         else:
             self.refresh_index()
-
-        # print('the_eye.py:TheEye:load_index: len(self.index) =', str(len(self.index)))
-
-        # return self.index
 
     def _get_links(self, url):
         """Requests a URL and parses hrefs from the result.
@@ -50,19 +40,12 @@ class TheEye:
         :param url: str pointing to a page within The Eye's /public/
         :return: list of hrefs on the page, within <pre>
         """
-        # print('the_eye.py:TheEye:_get_links: url =', url)
-
         r = urllib2.urlopen(urllib2.Request(
             url,
             headers={'User-agent':'Mozilla/5.0 (Windows NT 10.0; ) AppleWebKit/'
                                   '537.36 (KHTML, like Gecko) Chrome/83.0.4086.'
                                   '0 Safari/537.36'}
         ))
-
-        # While developing I used a stored response instead of
-        # constantly querying The Eye
-        # r = pickle.load(open('r.pkl', mode='rb'))
-        # pickle.dump(r, open('./r.pkl', mode='wb'))
 
         soup = BeautifulSoup(r.read(), 'html.parser')
         r.close()
@@ -87,8 +70,6 @@ class TheEye:
         :param url: str pointing to a page to start crawling from
         :return: list of URLs of files (i.e. URLs not ending in '/')
         """
-        # print('the_eye.py:TheEye:_crawl_links: url =', url)
-
         links = self._get_links(url)
 
         pages = [l for l in links if l[-1] == '/']
@@ -111,14 +92,10 @@ class TheEye:
         :return: the index, loaded as one-dimensional list of URLs of
                  files
         """
-        # print('the_eye.py:TheEye:refresh_index')
-
         self.index = self._crawl_links(self.base_url)
 
         with gzip.open(self.index_file, mode='wb') as index_json:
             index_json.write(json.dumps(self.index, indent=4))
-
-        # return self.index
 
     def search(self, query, mode='all', format='EPUB'):
         """Search the index for any or all words in the given query
@@ -129,8 +106,6 @@ class TheEye:
                      present to match. Defaults to 'all'
         :return: list of matching items
         """
-        # print('the_eye.py:TheEye:search: query =', query)
-
         keywords = query.split(' ')
 
         if mode == 'any':
@@ -161,4 +136,4 @@ if __name__ == '__main__':
     # Make sure to refresh the index after changing the base URL
     # eye.refresh_index()
 
-    print('\n'.join(eye.search('gandhi biography', format='MOBI')))
+    print('\n'.join(eye.search('mahatma gandhi', format='EPUB')))
